2d40eb754716723cf32e72e833595a9badf41055
chapmanCC <chapboy1989@gmail.com>
2019-09-13 02:44:00 +0800

    basic routing

--- app.js
+++ app.js
@@ -21,18 +21,43 @@ Books.sync()
 const app = express();
 const port = 3000;
 
-app.set("views", "views");
+app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => res.redirect("/books")); // need to redirect to "/books"
 app.get("/books", (req, res) => res.render("all_books"));
-// app.post("/books/new", (req, res) =>)
 
-// app.get("/books/:id", (req, res) =>)
-// app.post("/books/:id", (req, res) =>)
-// app.delete("/books/:id/delete", (req, res) =>)
+app
+  .route("/books/new")
+  .get((req, res) => res.render("new_book"))
+  .post((req, res) => {});
+
+app
+  .route("/books/:id")
+  .get((req, res) => {
+    const { id } = req.params;
+  })
+  .post((req, res) => {
+    const { id } = req.params;
+  });
+
+app.delete("/books/:id/delete", (req, res) => {
+  const { id } = req.params;
+});
+
+// ================================
+// *          ERROR HANDLER
+// ================================
+
+app.use((err, req, res, next) => {
+  // ? might need to expand the error object
+  console.error(err);
+  res.status(500).send("it's broken! Fix it");
+  // res.render("error", { err });
+  next();
+});
 
 app.listen(port, () => console.log(`App is listening on port ${port}`));
 
