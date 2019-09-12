97befacacb5342daadd9c78d335fd480572500cf
chapmanCC <chapboy1989@gmail.com>
2019-09-12 09:07:15 +0800

    minor updates on comments

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+/**
+ * * IMPORTS
+ */
+
 const express = require("express");
 const db = require("./db");
 
 const { Books } = db.models;
 
+/**
+ * * SEQUELIZE
+ */
 Books.sync()
   .then()
   .catch(err => console.error(`There is an error: ${err}`));
 
 /**
- * * EXPRESS
+ * * ROUTER
  */
 
 const app = express();
 const port = 3000;
+
+app.set("views", "views");
+app.set("view engine", "pug");
 
 // app.get("/", (req, res) => ); //need to redirect to "/books"
 // app.get("/books", (req, res) =>)
@@ -22,6 +32,12 @@ const port = 3000;
 
 // app.get("/books/:id", (req, res) =>)
 // app.post("/books/:id", (req, res) =>)
-// app.post("/books/:id/delete", (req, res) =>)
+// app.delete("/books/:id/delete", (req, res) =>)
 
 app.listen(port, () => console.log(`App is listening on port ${port}`));
+
+/**
+ * * == NOTES == *
+ * instructions for this project
+ * https://teamtreehouse.com/projects/sql-library-manager
+ */
